e50b69c65417b68a341b606398a98a75e1c86ab1
Marcelo <marcelopwmendes@gmail.com>
2020-01-19 00:13:42 +0000

    added models from database

---
+++ PatinhasMimadas/PatinhasMimadas.Models/Customer.cs
@@ -0,0 +1,35 @@
+namespace PatinhasMimadas.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Customer
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Customer()
+        {
+            Bookings = new HashSet<Booking>();
+        }
+
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(50)]
+        public string Email { get; set; }
+
+        public long Phone { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Password { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Booking> Bookings { get; set; }
+    }
+}
