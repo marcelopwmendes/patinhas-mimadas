37ca1d5ed5cdab30d56dba054f561c73126a60fd
marcelopwmendes <marcelopwmendes@gmail.com>
2020-03-23 22:03:10 +0000

    Project to .NET Core

---
+++ PatinhasMimadas/PatinhasMimadas.Data/Service.cs
@@ -0,0 +1,28 @@
+namespace PatinhasMimadas.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Service
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Service()
+        {
+            Bookings = new HashSet<Booking>();
+        }
+
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Booking> Bookings { get; set; }
+    }
+}
--- PatinhasMimadas/PatinhasMimadas.DataAccess/Class1.cs
+++ PatinhasMimadas/PatinhasMimadas.DataAccess/Class1.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PatinhasMimadas.DataAccess
 {
     public class Class1
     {
-
     }
 }
